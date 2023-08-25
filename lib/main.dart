8a044e64a94ff8a1888ba8eef04c204ffe4cde93
Martin Ballester <mballester@q4tech.com>
2023-08-25 19:23:55 -0300

    counter finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/screens/counter/counter_functions.dart';
 import 'package:untitled/screens/counter/counter_screen.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         colorSchemeSeed: Colors.blue,
       ),
-      home: CounterScreen()
+      home: const CounterFunctionsScreen()
     );
   }
 }
